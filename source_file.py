a3bd879aa19420d74372411f9b7d3ec895e0f168
driveloader <tranlongdo2506@gmail.com>
2024-08-27 16:54:16 +0700

    Update source_file.py
    My code

--- source_file.py
+++ source_file.py
@@ -1,163 +1,197 @@
-import tkinter
+import tkinter as tk
+from tkinter import ttk
+from tkinter.messagebox import showinfo
+from tkinter.filedialog import askopenfilename, asksaveasfilename
 import os
-import webbrowser
-from tkinter import *
-from tkinter.messagebox import *
-from tkinter.filedialog import *
+from tkinter.simpledialog import askstring
 
-class Notepad:
+class NotePy:
+    def __init__(self, **kwargs):
+        self.__root = tk.Tk()
+        self.__root.title("Untitled - NotePy")
 
-	__root = Tk()
-	# default window width and height
-	__thisWidth = 300
-	__thisHeight = 300
-	__thisTextArea = Text(__root)
-	__thisMenuBar = Menu(__root)
-	__thisFileMenu = Menu(__thisMenuBar, tearoff=0)
-	__thisEditMenu = Menu(__thisMenuBar, tearoff=0)
-	__thisHelpMenu = Menu(__thisMenuBar, tearoff=0)
-	# To add scrollbar
-	__thisScrollBar = Scrollbar(__thisTextArea)
-	__file = None
+        self.__thisWidth = kwargs.get('width', 600)
+        self.__thisHeight = kwargs.get('height', 400)
+        screenWidth = self.__root.winfo_screenwidth()
+        screenHeight = self.__root.winfo_screenheight()
+        left = (screenWidth / 2) - (self.__thisWidth / 2)
+        top = (screenHeight / 2) - (self.__thisHeight / 2)
+        self.__root.geometry(f'{self.__thisWidth}x{self.__thisHeight}+{int(left)}+{int(top)}')
 
-	def __init__(self,**kwargs):
-		# Set icon
-		try:
-				self.__root.wm_iconbitmap("Notepad.ico")
-		except:
-				pass
-		# Set window size (the default is 300x300)
-		try:
-			self.__thisWidth = kwargs['width']
-		except KeyError:
-			pass
+        try:
+            self.__root.iconbitmap("NotePy.ico")
+        except tk.TclError:
+            pass
 
-		try:
-			self.__thisHeight = kwargs['height']
-		except KeyError:
-			pass
+        self.__mainFrame = ttk.Frame(self.__root)
+        self.__mainFrame.grid(sticky='nsew')
 
-		# Set the window text
-		self.__root.title("Untitled - Notepad")
-		# Center the window
-		screenWidth = self.__root.winfo_screenwidth()
-		screenHeight = self.__root.winfo_screenheight()
-		# For left-alling
-		left = (screenWidth / 2) - (self.__thisWidth / 2)
-		# For right-allign
-		top = (screenHeight / 2) - (self.__thisHeight /2)
-		# For top and bottom
+        self.__statusFrame = ttk.Frame(self.__root)
+        self.__statusFrame.grid(row=1, column=0, sticky='ew')
 
-		self.__root.geometry('%dx%d+%d+%d' % (self.__thisWidth,
-		 									self.__thisHeight,
-											left, top))
-		# To make the textarea auto resizable
-		self.__root.grid_rowconfigure(0, weight=3)
-		self.__root.grid_columnconfigure(0, weight=3)
+        self.__thisTextArea = tk.Text(self.__mainFrame, wrap='word', font=('Arial', 12))
+        self.__thisScrollBar = ttk.Scrollbar(self.__mainFrame, orient='vertical', command=self.__thisTextArea.yview)
+        self.__thisTextArea.config(yscrollcommand=self.__thisScrollBar.set)
 
-		# Add controls (widget)
-		self.__thisTextArea.grid(sticky = N + E + S + W)
-		# To open new file
-		self.__thisFileMenu.add_command(label="New",
-										command=self.__newFile)
-		# To open a already existing file
-		self.__thisFileMenu.add_command(label="Open",
-										command=self.__openFile)
-		# To save current file
-		self.__thisFileMenu.add_command(label="Save",
-										command=self.__saveFile)
-		# To create a line in the dialog
-		self.__thisFileMenu.add_separator()
-		self.__thisFileMenu.add_command(label="Exit",
-										command=self.__quitApplication)
-		self.__thisMenuBar.add_cascade(label="File",
-									menu=self.__thisFileMenu)
-		# To give a feature of cut
-		self.__thisEditMenu.add_command(label="Cut",
-										command=self.__cut)
-		# to give a feature of copy
-		self.__thisEditMenu.add_command(label="Copy",
-										command=self.__copy)
-		# To give a feature of paste
-		self.__thisEditMenu.add_command(label="Paste",
-										command=self.__paste)
-		# To give a feature of editing
-		self.__thisMenuBar.add_cascade(label="Edit",
-									menu=self.__thisEditMenu)
-		# To create a feature of description of the notepad
-		self.__thisHelpMenu.add_command(label="View Help",
-										command=self.__showAbout)
-		self.__thisMenuBar.add_cascade(label="Help",
-									menu=self.__thisHelpMenu)
-		self.__root.config(menu=self.__thisMenuBar)
-		self.__thisScrollBar.pack(side=RIGHT,fill=Y)
-		# Scrollbar will adjust automatically according to the content
-		self.__thisScrollBar.config(command=self.__thisTextArea.yview)
-		self.__thisTextArea.config(yscrollcommand=self.__thisScrollBar.set)
-	def __quitApplication(self):
-		self.__root.destroy()
-		# exit()
-	def __showAbout(self):
-		showinfo("Notepad",view =webbrowser.open("https://www.bing.com/search?q=get+help+with+notepad+in+windows+10&filters=guid:%224466414-en-dia%22%20lang:%22en%22&form=T00032&ocid=HelpPane-BingIA"))
-	def __openFile(self):
-		self.__file = askopenfilename(defaultextension=".txt",
-									filetypes=[("All Files","*.*"),
-										("Text Documents","*.txt")])
-		if self.__file == "":
-			# no file to open
-			self.__file = None
-		else:
-			# Try to open the file
-			# set the window title
-			self.__root.title(os.path.basename(self.__file) + " - Notepad")
-			self.__thisTextArea.delete(1.0,END)
-			file = open(self.__file,"r")
-			self.__thisTextArea.insert(1.0,file.read())
-			file.close()
+        self.__thisTextArea.grid(row=0, column=0, sticky='nsew')
+        self.__thisScrollBar.grid(row=0, column=1, sticky='ns')
 
-	def __newFile(self):
-		self.__root.title("Untitled - Notepad")
-		self.__file = None
-		self.__thisTextArea.delete(1.0,END)
+        self.__mainFrame.grid_rowconfigure(0, weight=1)
+        self.__mainFrame.grid_columnconfigure(0, weight=1)
 
-	def __saveFile(self):
+        self.__statusBar = tk.Label(self.__statusFrame, text="Ln 1 | Col 1 | 100% | CRLF | UTF-8", anchor='w')
+        self.__statusBar.grid(row=0, column=0, sticky='ew')
 
-		if self.__file == None:
-			# Save as new file
-			self.__file = asksaveasfilename(initialfile='Untitled.txt',
-											defaultextension=".txt",
-											filetypes=[("All Files","*.*"),
-												("Text Documents","*.txt")])
+        self.__thisMenuBar = tk.Menu(self.__root)
+        self.__root.config(menu=self.__thisMenuBar)
 
-			if self.__file == "":
-				self.__file = None
-			else:
-				# Try to save the file
-				file = open(self.__file,"Untitled")
-				file.write(self.__thisTextArea.get(1.0,END))
-				file.close()
+        self.__thisFileMenu = tk.Menu(self.__thisMenuBar, tearoff=0)
+        self.__thisFileMenu.add_command(label="New", command=self.__newFile, accelerator="Ctrl+N")
+        self.__thisFileMenu.add_command(label="Open", command=self.__openFile, accelerator="Ctrl+O")
+        self.__thisFileMenu.add_command(label="Save", command=self.__saveFile, accelerator="Ctrl+S")
+        self.__thisFileMenu.add_separator()
+        self.__thisFileMenu.add_command(label="Exit", command=self.__quitApplication, accelerator="Ctrl+Q")
+        self.__thisMenuBar.add_cascade(label="File", menu=self.__thisFileMenu)
 
-				# Change the window title
-				self.__root.title(os.path.basename(self.__file) + " - Notepad")
+        self.__thisEditMenu = tk.Menu(self.__thisMenuBar, tearoff=0)
+        self.__thisEditMenu.add_command(label="Cut", command=self.__cut, accelerator="Ctrl+X")
+        self.__thisEditMenu.add_command(label="Copy", command=self.__copy, accelerator="Ctrl+C")
+        self.__thisEditMenu.add_command(label="Paste", command=self.__paste, accelerator="Ctrl+V")
+        self.__thisEditMenu.add_command(label="Undo", command=self.__undo, accelerator="Ctrl+Z")
+        self.__thisEditMenu.add_command(label="Redo", command=self.__redo, accelerator="Ctrl+Y")
+        self.__thisEditMenu.add_command(label="Find", command=self.__findReplace, accelerator="Ctrl+F")
+        self.__thisEditMenu.add_command(label="Replace", command=self.__findReplace, accelerator="Ctrl+H")
+        self.__thisMenuBar.add_cascade(label="Edit", menu=self.__thisEditMenu)
 
-		else:
-			file = open(self.__file,"w")
-			file.write(self.__thisTextArea.get(1.0,END))
-			file.close()
+        self.__thisFormatMenu = tk.Menu(self.__thisMenuBar, tearoff=0)
+        self.__thisFormatMenu.add_command(label="Font Size", command=self.__setFontSize)
+        self.__thisFormatMenu.add_command(label="Font Style", command=self.__setFontStyle)
+        self.__thisMenuBar.add_cascade(label="Format", menu=self.__thisFormatMenu)
 
-	def __cut(self):
-		self.__thisTextArea.event_generate("<<Cut>>")
+        self.__thisHelpMenu = tk.Menu(self.__thisMenuBar, tearoff=0)
+        self.__thisHelpMenu.add_command(label="About NotePy", command=self.__showAbout)
+        self.__thisMenuBar.add_cascade(label="Help", menu=self.__thisHelpMenu)
 
-	def __copy(self):
-		self.__thisTextArea.event_generate("<<Copy>>")
+        self.__file = None
+        self.__line_endings = "CRLF"
+        self.__encoding = "UTF-8"
 
-	def __paste(self):
-		self.__thisTextArea.event_generate("<<Paste>>")
+        self.__thisTextArea.bind('<KeyRelease>', self.__updateStatusBar)
+        self.__root.bind_all('<Control-n>', lambda e: self.__newFile())
+        self.__root.bind_all('<Control-o>', lambda e: self.__openFile())
+        self.__root.bind_all('<Control-s>', lambda e: self.__saveFile())
+        self.__root.bind_all('<Control-q>', lambda e: self.__quitApplication())
+        self.__root.bind_all('<Control-x>', lambda e: self.__cut())
+        self.__root.bind_all('<Control-c>', lambda e: self.__copy())
+        self.__root.bind_all('<Control-v>', lambda e: self.__paste())
+        self.__root.bind_all('<Control-z>', lambda e: self.__undo())
+        self.__root.bind_all('<Control-y>', lambda e: self.__redo())
+        self.__root.bind_all('<Control-f>', lambda e: self.__findReplace())
+        self.__root.bind_all('<Control-h>', lambda e: self.__findReplace())
 
-	def run(self):
-		# Run main application
-		self.__root.mainloop()
+    def __quitApplication(self):
+        self.__root.destroy()
 
-# Run main application
-notepad = Notepad(width=600,height=400)
-notepad.run()
+    def __showAbout(self):
+        showinfo("NotePy", "Having trust issue with Microsoft Products? Here you are, a FREE and OPEN SOURCE Notepad created in Python! Created by: Long Do (http://longdo.pythonanywhere.com/)")
+
+    def __openFile(self):
+        self.__file = askopenfilename(defaultextension=".txt", filetypes=[("All Files", "*.*"), ("Text Documents", "*.txt")])
+        if self.__file:
+            self.__root.title(os.path.basename(self.__file) + " - NotePy")
+            self.__thisTextArea.delete(1.0, tk.END)
+            with open(self.__file, "r", encoding=self.__encoding) as file:
+                self.__thisTextArea.insert(1.0, file.read())
+            self.__updateStatusBar()
+
+    def __newFile(self):
+        self.__root.title("Untitled - NotePy")
+        self.__file = None
+        self.__thisTextArea.delete(1.0, tk.END)
+        self.__updateStatusBar()
+
+    def __saveFile(self):
+        if self.__file is None:
+            self.__file = asksaveasfilename(initialfile='Untitled.txt', defaultextension=".txt", filetypes=[("All Files", "*.*"), ("Text Documents", "*.txt")])
+        if self.__file:
+            with open(self.__file, "w", encoding=self.__encoding) as file:
+                file.write(self.__thisTextArea.get(1.0, tk.END))
+            self.__root.title(os.path.basename(self.__file) + " - NotePy")
+
+    def __cut(self):
+        self.__thisTextArea.event_generate("<<Cut>>")
+
+    def __copy(self):
+        self.__thisTextArea.event_generate("<<Copy>>")
+
+    def __paste(self):
+        self.__thisTextArea.event_generate("<<Paste>>")
+
+    def __undo(self):
+        self.__thisTextArea.event_generate("<<Undo>>")
+
+    def __redo(self):
+        self.__thisTextArea.event_generate("<<Redo>>")
+
+    def __findReplace(self):
+        findReplaceWindow = tk.Toplevel(self.__root)
+        findReplaceWindow.title("Find and Replace")
+        tk.Label(findReplaceWindow, text="Find:").grid(row=0, column=0, sticky='e')
+        findEntry = tk.Entry(findReplaceWindow, width=30)
+        findEntry.grid(row=0, column=1, padx=5, pady=5)
+        tk.Label(findReplaceWindow, text="Replace with:").grid(row=1, column=0, sticky='e')
+        replaceEntry = tk.Entry(findReplaceWindow, width=30)
+        replaceEntry.grid(row=1, column=1, padx=5, pady=5)
+        tk.Button(findReplaceWindow, text="Find", command=lambda: self.__find(findEntry.get())).grid(row=2, column=0, padx=5, pady=5)
+        tk.Button(findReplaceWindow, text="Replace", command=lambda: self.__replace(findEntry.get(), replaceEntry.get())).grid(row=2, column=1, padx=5, pady=5)
+        tk.Button(findReplaceWindow, text="Cancel", command=findReplaceWindow.destroy).grid(row=2, column=2, padx=5, pady=5)
+
+    def __find(self, search_text):
+        content = self.__thisTextArea.get(1.0, tk.END)
+        index = content.find(search_text)
+        if index != -1:
+            self.__thisTextArea.mark_set("insert", f"1.0+{index}c")
+            self.__thisTextArea.see("insert")
+
+    def __replace(self, search_text, replace_text):
+        content = self.__thisTextArea.get(1.0, tk.END)
+        new_content = content.replace(search_text, replace_text)
+        self.__thisTextArea.delete(1.0, tk.END)
+        self.__thisTextArea.insert(1.0, new_content)
+
+    def __setFontSize(self):
+        size = askstring("Font Size", "Enter font size:")
+        if size:
+            try:
+                size = int(size)
+                current_font = self.__thisTextArea.cget("font")
+                font_family, font_size = current_font.split(' ')
+                self.__thisTextArea.config(font=(font_family, size))
+            except ValueError:
+                showinfo("Error", "Invalid font size.")
+
+    def __setFontStyle(self):
+        font_style = askstring("Font Style", "Enter font style (e.g., Arial, Courier):")
+        if font_style:
+            current_font = self.__thisTextArea.cget("font")
+            font_family, font_size = current_font.split(' ')
+            self.__thisTextArea.config(font=(font_style, font_size))
+
+    def __updateStatusBar(self, event=None):
+        content = self.__thisTextArea.get(1.0, tk.END)
+        num_lines = int(self.__thisTextArea.index('end-1c').split('.')[0])
+        num_cols = len(self.__thisTextArea.get(tk.INSERT + " linestart", tk.INSERT))
+        num_words = len(content.split())
+        
+        line, col = map(int, self.__thisTextArea.index(tk.INSERT).split('.'))
+        line_endings = self.__line_endings
+        encoding = self.__encoding
+        
+        self.__statusBar.config(text=f"Ln {line} | Col {col} | {num_words} Words | {line_endings} | {encoding}")
+
+    def run(self):
+        self.__root.mainloop()
+
+if __name__ == "__main__":
+    NotePy = NotePy(width=600, height=400)
+    NotePy.run()
